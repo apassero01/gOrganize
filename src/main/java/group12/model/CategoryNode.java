a3a1df39b642ccd7fab24d2161a6913fc495d673
Andrew Passero <ajp031@bucknell.edu>
2022-11-06 13:52:46 -0500

    initial categoryNode implementation

---
+++ src/main/java/group12/model/CategoryNode.java
@@ -0,0 +1,15 @@
+/* ***************************************** 
+ * CSCI205 -Software Engineering and Design 
+ * Fall2022* Instructor: Prof. Brian King
+ *
+ * Name: Andrew Passero
+ * Section: Section 2 11am
+ * Date: 11/6/22* Time: 1:41 PM 
+ *  
+ * Project: csci205_final_project
+ * Package: group12.model
+ * Class: CategoryNode
+ *
+ * Description: 
+ *  
+ * *****************************************/package group12.model;public class CategoryNode {}
