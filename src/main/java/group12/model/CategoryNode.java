4491f35287d788dda3a2d6dba3fd3e28d905d835
Andrew Passero <ajp031@bucknell.edu>
2022-11-06 13:53:24 -0500

    initial categoryNode implementation

--- src/main/java/group12/model/CategoryNode.java
+++ src/main/java/group12/model/CategoryNode.java
@@ -1,15 +1,50 @@
-/* ***************************************** 
- * CSCI205 -Software Engineering and Design 
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
  * Fall2022* Instructor: Prof. Brian King
  *
  * Name: Andrew Passero
  * Section: Section 2 11am
- * Date: 11/6/22* Time: 1:41 PM 
- *  
+ * Date: 11/6/22* Time: 1:41 PM
+ *
  * Project: csci205_final_project
  * Package: group12.model
  * Class: CategoryNode
  *
- * Description: 
- *  
- * *****************************************/package group12.model;public class CategoryNode {}
+ * Description:
+ *
+ * *****************************************/
+package group12.model;
+
+import java.util.TreeMap;
+
+public class CategoryNode
+{
+    private String name;
+    private TreeMap<String, CategoryNode> childrenCategories;
+    private String desciption;
+
+    public CategoryNode(String name,String description)
+    {
+        this.name = name;
+        this.childrenCategories = new TreeMap<>();
+        this.desciption = description;
+    }
+
+    public void addCategory(String name,String desciption)
+    {
+        CategoryNode newCategory = new CategoryNode(name,desciption);
+        this.childrenCategories.put(name,newCategory);
+
+
+    }
+    public void addDescription(String description)
+    {
+        this.desciption = description;
+    }
+
+    public CategoryNode getNextCategory(String name)
+    {
+        return childrenCategories.get(name);
+    }
+
+}
